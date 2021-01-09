1ecc18f25da6241192f9a2f7e3228ffab2e470dc
Sahil Mahendrakar <sahilmahendrakar@Sahils-MacBook-Pro.local>
2021-01-09 17:07:04 -0500

    initial commit

---
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedimage/CustomArFragment.java
@@ -0,0 +1,8 @@
+package com.google.ar.core.examples.java.augmentedimage;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class CustomArFragment extends ArFragment {
+
+
+}
---
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedimage/DrawActivity.java
@@ -0,0 +1,13 @@
+package com.google.ar.core.examples.java.augmentedimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
